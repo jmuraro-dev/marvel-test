61882f67064dd6f22c803fdbf26b7833a476db08
jeffmuraro-dev <jeff@muraro.dev>
2021-10-02 15:10:28 +0200

    Creation of a screen for the homepage

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+import '../../assets/styles/shared.scss';
+import './HomeScreen.scss';
+
+import ComicCard from "../../components/ComicCard";
+import {getComics} from "../../api/Comics";
+
+class HomeScreen extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            comics: []
+        }
+    }
+
+    async componentDidMount() {
+        const comics = await getComics()
+
+        await this.setState({comics})
+    }
+
+    render() {
+        return (
+            <div className="container">
+                {this.state.comics.length != 0 ? this.state.comics.map((comic, key) => (
+                    <ComicCard key={key} title={comic.title} description={comic.description} thumbnail={comic.thumbnail} />
+                )) : null}
+            </div>
+        );
+    }
+}
+
+export default HomeScreen;
