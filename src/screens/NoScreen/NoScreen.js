0d57c2cc5d44e9bd3ace06bb11a4b3ad2f9e8e27
jeffmuraro-dev <jeff@muraro.dev>
2021-10-02 15:11:50 +0200

    Creation of a screen for the 404 page

---
+++ src/screens/NoScreen/NoScreen.js
@@ -0,0 +1,11 @@
+function NoScreen() {
+    return (
+        <div>
+            <h3>
+                No page here. It seems Thanos was here and have delete all sign of life.
+            </h3>
+        </div>
+    )
+}
+
+export default NoScreen;
