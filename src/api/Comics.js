5dae1c28770c1c7d2ca3070890665f3ca707d59f
jeffmuraro-dev <jeff@muraro.dev>
2021-10-04 15:58:13 +0200

    Creation of button to get comics randomly

--- src/api/Comics.js
+++ src/api/Comics.js
@@ -1,27 +1,20 @@
 import {API_BASE_URL, API_PARAMS_URL} from "./config";
 
-export async function getComics(max, offset) {
-    if (max === undefined)
-        max = 10
-    else if (max > 10)
-        max = 10
-    else if (max < 0)
-        max = 1
-
+export async function getComics(offset) {
     if (offset === undefined)
         offset = 0
 
     const url =
         API_BASE_URL +
         '/v1/public/comics?format=comic&noVariants=true&' +
-        'limit=' + max + '&' +
+        'limit=' + 10 + '&' +
         'offset=' + offset + '&' +
         API_PARAMS_URL
 
     try {
         const response = await fetch(url)
         const json = await response.json()
-        return json.data.results
+        return json.data
     } catch (err) {
         console.error("Fetch Error for getting comics -----", err)
         return null
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,33 +1,64 @@
 import React, {Component} from 'react';
 
 import '../../assets/styles/shared.scss';
+import './HomeScreen.scss'
 
 import ComicCard from "../../components/ComicCard";
+import { UilArrowRandom } from "@iconscout/react-unicons/icons/uil-arrow-random"
+
 import {getComics} from "../../api/Comics";
+import UilAngleLeft from "@iconscout/react-unicons/icons/uil-arrow-random";
 
 class HomeScreen extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            comics: []
+            comics: [],
+            comicsTotal: 0,
+            loading: true
         }
     }
 
     async componentDidMount() {
         const comics = await getComics()
+        await this.setState({comics: comics.results, comicsTotal: comics.total, loading: false})
+    }
 
-        await this.setState({comics})
+    getNewComics = async () => {
+        this.setState({loading: true})
+
+        const min = Math.ceil(0)
+        const max = Math.floor(this.state.comicsTotal / 10)
+        const offset = Math.floor(Math.random() * (max - min)) + min
+
+        const comics = await getComics(offset)
+        await this.setState({comics: comics.results, comicsTotal: comics.total, loading: false})
     }
 
     render() {
-        return (
-            <div className="container">
-                {this.state.comics.length !== 0 ? this.state.comics.map((comic, key) => (
-                    <ComicCard key={key} id={comic.id} title={comic.title} description={comic.description} thumbnail={comic.thumbnail} />
-                )) : null}
-            </div>
-        );
+        if (this.state.loading) {
+            return (
+                <div className="container">
+                    <h1>Loading data ...</h1>
+                </div>
+            )
+        } else {
+            return (
+                <div className="container">
+                    <button onClick={this.getNewComics} className="button__random button__random__flex random__button">
+                        Other random comics
+                    </button>
+
+                    <div className="home__content">
+                        {this.state.comics.length !== 0 ? this.state.comics.map((comic, key) => (
+                            <ComicCard key={key} id={comic.id} title={comic.title} description={comic.description}
+                                       thumbnail={comic.thumbnail}/>
+                        )) : null}
+                    </div>
+                </div>
+            );
+        }
     }
 }
 
