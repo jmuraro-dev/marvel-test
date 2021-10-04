d686823eeb9a8ed3b22384dc70771056f33a4cad
jeffmuraro-dev <jeff@muraro.dev>
2021-10-04 14:06:03 +0200

    Management of the footer to display it on the end of the page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.scss'
 function Footer() {
     return (
         <footer className="footer">
-            <p className="footer__copy">&#169; Muraro Jeff. Data provided by Marvel. &#169; 2014 Marvel</p>
+            <p className="footer__copy">&#169; Muraro Jeff. <br /> Data provided by Marvel. &#169; 2014 Marvel</p>
         </footer>
     )
 }
